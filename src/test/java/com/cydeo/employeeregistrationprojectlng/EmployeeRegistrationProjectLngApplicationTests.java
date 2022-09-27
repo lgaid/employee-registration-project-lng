a93fe5abf92a6ef060cd857514ecd56e42f15624
lgaid <104643045+lgaid@users.noreply.github.com>
2022-09-27 16:21:22 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/employeeregistrationprojectlng/EmployeeRegistrationProjectLngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistrationprojectlng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationProjectLngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
